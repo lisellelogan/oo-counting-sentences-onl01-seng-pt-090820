6e9546e46321c9533dfa48f4d343901623654e36
Liselle Logan <liselle.logan@gmail.com>
2020-09-26 19:06:27 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    self.split(/\.|\?|\!/).delete_if{|w|w.size < 2}.size
-  #binding.pry
+    self.split(/\.|\?|\!/).delete_if{|w|w.size < 2}
+  binding.pry
   end
 end
